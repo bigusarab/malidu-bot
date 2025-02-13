0033f367ca44ccc33c28932600242493b65c8977
bigusarabnme <nethmi2474000@gmail.com>
2025-02-13 09:13:08 +0530

    Update config .js

--- config .js
+++ config .js
@@ -7,3 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 };
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9743acae8d366e4efe31c-48d3a1fdcd5f284970.jpg"
+ALIVE_MSG: brocess.env.ALIVE_MSG ||"රාජ කුමාරයෙක් නෙමේ මම දකුනෙ කොල්ලෙක්",
